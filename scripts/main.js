b83709be11a133a9467465d0042ffa200871779f
josh <josh.pontes99@gmail.com>
2021-01-06 17:18:51 -0600

    added red/black options

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
 
-    dice3d.addSystem({id: "Ranger11Danger", name: "Ranger11Danger"}, false);
+    dice3d.addSystem({id: "Ranger11Danger-black", name: "Ranger11Danger-black"}, false);
+    dice3d.addSystem({id: "Ranger11Danger-red", name: "Ranger11Danger-red"}, false);
 	dice3d.addDicePreset({
     type: "d20",
     labels: [
-      "modules/Ranger11Danger/textures/fuck_text.webp",
+      "modules/Ranger11Danger/textures/fuck_text_black.webp",
       "2",
       "3",
       "4",
@@ -23,10 +24,39 @@ Hooks.on('diceSoNiceReady', (dice3d) => {
       "17",
       "18",
       "19",
-	  "modules/Ranger11Danger/textures/yeet_text.webp"
+	  "modules/Ranger11Danger/textures/yeet_text_black.webp"
     ],
 	bumpMaps: ["modules/Ranger11Danger/textures/fuck_text_bump.webp",,,,,,,,,,,,,,,,,,,"modules/Ranger11Danger/textures/yeet_text_bump.webp"],
-    system: "Ranger11Danger",
+    system: "Ranger11Danger-black",
+	fontScale: 1.0
+   },"d20");
+
+   dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "modules/Ranger11Danger/textures/fuck_text_red.webp",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+	  "modules/Ranger11Danger/textures/yeet_text_red.webp"
+    ],
+	bumpMaps: ["modules/Ranger11Danger/textures/fuck_text_bump.webp",,,,,,,,,,,,,,,,,,,"modules/Ranger11Danger/textures/yeet_text_bump.webp"],
+    system: "Ranger11Danger-red",
 	fontScale: 1.0
    },"d20");
 
