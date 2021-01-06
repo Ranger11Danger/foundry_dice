87984289b96eab74e7028abe2b6be12329909691
josh <josh.pontes99@gmail.com>
2021-01-06 00:41:08 -0600

    added main files
    only added "fuck" dice

---
+++ Ranger11Danger_Dice/scripts/main.js
@@ -0,0 +1,33 @@
+Hooks.on('diceSoNiceReady', (dice3d) => {
+
+    dice3d.addSystem({id: "Ranger11Danger", name: "Ranger11Danger"}, false);
+	dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "modules/Ranger11Danger_Dice/textures/fuck_text.webp",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+	  "20"
+    ],
+	bumpMaps: ["modules/Ranger11Danger_Dice/textures/fuck_text_bump.webp",,,,,,,,,,,,,,,,,,,],
+    system: "Ranger11Danger",
+	fontScale: 1.0
+   },"d20");
+
+});
